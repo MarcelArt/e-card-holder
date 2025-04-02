85dd76dd8bc48c25026e83c20d6d9d2ca3fe90da
MarcelArt <badman.islemon@gmail.com>
2025-04-03 01:12:33 +0700

    setup gluestack

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="try"
+        options={{
+          title: 'Try',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="flask.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
