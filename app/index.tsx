21e56774972cc257c31f2ccd9e68eea8b860736a
MarcelArt <badman.islemon@gmail.com>
2025-04-03 17:10:19 +0700

    Setup sqlite and lucide

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import ProfileComponent from '@/components/ProfileComponent';
 import { Box } from '@/components/ui/box';
+import { Fab, FabIcon, FabLabel } from '@/components/ui/fab';
 import { Heading } from '@/components/ui/heading';
 import { VStack } from '@/components/ui/vstack';
 import { FlatList } from 'react-native';
+import { Plus } from 'lucide-react-native';
 
 export default function ProfilesView() {
 	const datas = [
@@ -17,12 +19,15 @@ export default function ProfilesView() {
 	];
 
 	return (
-		<VStack className="mt-16 mx-2">
-			<Heading size="3xl">Pilih profil</Heading>
-			<FlatList
-				data={datas}
-				renderItem={({ item }) => <ProfileComponent id={item.id} name={item.name} />}
-			/>
-		</VStack>
+		<Box className='h-full'>
+			<VStack className="mt-16 mx-2">
+				<Heading size="3xl">Pilih profil</Heading>
+				<FlatList data={datas} renderItem={({ item }) => <ProfileComponent id={item.id} name={item.name} />} />
+			</VStack>
+			<Fab size="md" placement="bottom right" isHovered={false} isDisabled={false} isPressed={false}>
+				<FabIcon as={Plus} />
+				<FabLabel>Quick start</FabLabel>
+			</Fab>
+		</Box>
 	);
 }
